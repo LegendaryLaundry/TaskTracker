e3ccc5574d871b05b8e7a56414ea8c8e041c633c
Morgan Nwosu <cloudofmorgan@gmail.com>
2021-12-15 09:53:08 -0800

    titled

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import Header from './components/Header';
 import TaskForm from './components/TaskForm';
 import TaskList
  from './components/TaskList';
+ import { Helmet } from 'react-helmet'
 function App() {
 	const [task, setTask] = useState([]);
 	const [totalTaskCount, setTotalTaskCount] = useState(0);
+  const TITLE = 'Accomplishment Tracker';
  
 
   useEffect(() => {
@@ -19,6 +21,11 @@ function App() {
 
 	return (
 		<div className="App">
+      <>
+        <Helmet>
+          <title>{ TITLE }</title>
+        </Helmet>
+      </>
 			<Header totalTaskCount={totalTaskCount} />
       <TaskForm task={task} setTask={setTask}/>
       <TaskList task={task} setTask={setTask}/>
